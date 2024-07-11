e9cdf0deb468d9908141ee2d0502bf3c7b792b1d
Ahmed Yousry <ahmedu3helal@gmail.com>
2024-07-11 22:08:49 +0300

    update container

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-// when i click to the button concat the number in input field with link like this https://wa.me/number then redirect to the link
-// the number should be like this expression ^01[0-2,5]{1}[0-9]{8}$
-// if the number is not valid show error under input field message
-// if the number is valid redirect to the link
 const form = document.querySelector('form');
 const input = document.querySelector('input[name="number"]');
 const errorMessage = document.querySelector('#error-message');
